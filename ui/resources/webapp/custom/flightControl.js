bfcee0f2008be4eca36a2e045cd542070349371a
lalugeo <lalu.george@ifb-group.com>
2019-01-31 08:55:38 +0000

    removed unneccesary files

--- ui/resources/webapp/custom/flightControl.js
+++ ui/resources/webapp/custom/flightControl.js
@@ -1,4 +1,4 @@
-sap.ui.core.Control.extend("mh.Flight", {
+sap.ui.core.Control.extend("mh.custom.Flight", {
     renderer : function(docRef) {
         docRef.write('<span style="font-size: 128px;">&#9992;</span>');
     }
